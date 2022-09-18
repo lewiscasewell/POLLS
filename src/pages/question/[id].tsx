39834c10fca3b046ab7ad52b2e59293532d9c991
lewiscasewell <email>
2022-09-18 13:41:02 +0100

    add back button for question page

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 
@@ -9,13 +10,21 @@ const QuestionPageContent: React.FC<{ id: string }> = ({ id }) => {
   }
 
   return (
-    <div className="p-8 flex flex-col h-full min-h-screen">
+    <div className="p-8 flex flex-col h-full min-h-screen gap-2">
+      <Link href="/">
+        <a>Back</a>
+      </Link>
       {data?.isOwner && <div>You made this question!</div>}
-      <h1 className="font-bold text-2xl">{data?.question?.question}</h1>
       <div>
-        {(data?.question?.options as string[])?.map((option) => (
-          <div key={(option as any).text}>{(option as any).text}</div>
-        ))}
+        <h1 className="font-bold text-2xl">
+          {data?.question?.question}
+          {`${data?.question?.question.includes("?") ? "" : "?"}`}
+        </h1>
+        <div>
+          {(data?.question?.options as string[])?.map((option) => (
+            <div key={(option as any).text}>{(option as any).text}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
